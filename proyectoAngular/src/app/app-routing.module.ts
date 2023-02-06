d112b5ee4434a323f4d8b4b7a50b8e464bb625f5
usuario3jesus <jesusmoralescanete@gmail.com>
2023-02-06 21:54:27 +0100

    commit

--- proyectoAngular/src/app/app-routing.module.ts
+++ proyectoAngular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { PaginaNoEncontradaComponent } from './views/pagina-no-encontrada/pagina-no-encontrada.component';
+import { AcercaDeComponent } from './views/acerca-de/acerca-de.component';
+import { ListadoComponent } from './views/listado/listado.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'listado', component: ListadoComponent},
+  {path: 'acercaDe', component: AcercaDeComponent},
+
+  {path: '', redirectTo: '/listado', pathMatch: 'full'},
+  {path: '**', component: PaginaNoEncontradaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- proyectoAngular/src/app/app.module.ts
+++ proyectoAngular/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { ListadoComponent } from './views/listado/listado.component';
 import { EntradaComponent } from './views/listado/entrada/entrada.component';
+import { PaginaNoEncontradaComponent } from './views/pagina-no-encontrada/pagina-no-encontrada.component';
+import { AcercaDeComponent } from './views/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     ListadoComponent,
-    EntradaComponent
+    EntradaComponent,
+    PaginaNoEncontradaComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
